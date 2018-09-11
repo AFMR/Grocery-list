e197d0ef43f6daa37c7cdfabf638d132082e58f4
Eury <32747825+AFMR@users.noreply.github.com>
2018-09-11 16:11:11 -0700

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppGroceryComponent } from './app-grocery/app-grocery.component';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 const routes: Routes = [
   {
-    path: 'list',
-    component: AppGroceryComponent ,
+    path: '',
+    component: LandingPageComponent ,
+  }
+  {
+    path:'list',
+    component: AppGroceryComponent,
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppGroceryComponent } from './app-grocery/app-grocery.component';
 import { HeaderComponent } from './header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AppGroceryComponent,
-    HeaderComponent
+    HeaderComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
@@ -23,3 +26,4 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   bootstrap: [AppComponent],
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
